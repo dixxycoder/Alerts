9edbffff62f379fd8c5c044b87688066fd34b0ac
Grant Spraker <grant.spraker@gmail.com>
2021-10-25 10:43:20 -0600

    done with Alert.

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -1,19 +1,37 @@
-//
-//  ViewController.swift
-//  Alerts
-//
-//  Created by Grant Spraker on 10/25/21.
-//
+/*
+ViewController.swift
+Alerts
+
+Created by Grant Spraker on 10/25/21.
+*/
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func addAtction(_ sender: Any)
+    {
+        //Create Alert
+        let alert = UIAlertController (title: "Alert!", message: "enter text, RIGHT NOW", preferredStyle: .alert)
+        
+        //Put OK Button on Alert
+        let ok = UIAlertAction (title: "OK", style: .default, handler: nil)
 
+        alert.addAction(ok)
+        
+        //Cancel button alert
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
